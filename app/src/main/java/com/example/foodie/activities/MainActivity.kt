3e9f991b5ff9d7f38f31cebb2a71b6505457dea9
josipfrljic94 <josipfrljic77@gmail.com>
2023-12-14 00:22:10 +0100

    feat: connect firebase db

--- app/src/main/java/com/example/foodie/activities/MainActivity.kt
+++ app/src/main/java/com/example/foodie/activities/MainActivity.kt
@@ -5,10 +5,17 @@ import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.example.foodie.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.database
 
 
  class MainActivity : AppCompatActivity() {
      private val TAG = "MainActivity"
+     private lateinit var database: DatabaseReference
+// ...
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,6 +24,12 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
         val navController= Navigation.findNavController(this, R.id.host_fragment)
 
         NavigationUI.setupWithNavController(bottomNavigation,navController)
+
+        val database = FirebaseDatabase.getInstance().reference
+        val message = Message("John", "Hello, world!")
+        database.child("messages").push().setValue(message)
     }
 
 }
+
+ data class Message(val author: String, val content: String)
